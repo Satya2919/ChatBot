17bb9aa14cd6f32c2a0416f736f93b03e7dbeb66
satyam <sat.tiwari21@gmail.com>
2020-06-11 20:28:10 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,217 @@
+import 'dart:convert';
+import 'dart:developer';
+import 'dart:io';
+import 'package:fluttertoast/fluttertoast.dart';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Example Dialogflow Flutter',
+      theme: new ThemeData(
+        primarySwatch: Colors.deepOrange,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: new HomePageDialogflow(),
+    );
+  }
+}
+
+class HomePageDialogflow extends StatefulWidget {
+  HomePageDialogflow({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _HomePageDialogflow createState() => new _HomePageDialogflow();
+}
+
+class _HomePageDialogflow extends State<HomePageDialogflow> {
+
+  Icon callIcon = new Icon(Icons.call);
+  final List<ChatMessage> _messages = <ChatMessage>[];
+  final TextEditingController _textController = new TextEditingController();
+
+  Widget _buildTextComposer() {
+    return new IconTheme(
+      data: new IconThemeData(color: Theme.of(context).accentColor),
+      child: new Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: new Row(
+          children: <Widget>[
+            new Flexible(
+              child: new TextField(
+                controller: _textController,
+                onSubmitted: _handleSubmitted,
+                decoration:
+                new InputDecoration.collapsed(hintText: "Send a message"),
+              ),
+            ),
+            new Container(
+              margin: new EdgeInsets.symmetric(horizontal: 4.0),
+              child: new IconButton(
+                  icon: new Icon(Icons.send),
+                  onPressed: () => _handleSubmitted(_textController.text)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void request(String msg) async{
+    String url =
+        'http://192.168.0.101:3000/api/v1/bots/newbot/converse/userId';
+    Map map = {
+      'type': 'text',
+      'text':msg
+    };
+    print(await apiRequest(url, map));
+  }
+  Future<String> apiRequest(String url, Map jsonMap) async {
+    HttpClient httpClient = new HttpClient();
+    HttpClientRequest request = await httpClient.postUrl(Uri.parse(url));
+    request.headers.set('content-type', 'application/json');
+    request.add(utf8.encode(json.encode(jsonMap)));
+    HttpClientResponse response = await request.close();
+    // todo - you should check the response.statusCode
+    String reply = await response.transform(utf8.decoder).join();
+    Map<String, dynamic> parsedJson = json.decode(reply);
+    List<dynamic> data = parsedJson["responses"];
+    print(data[0]['type']);
+    for (var i = 0; i<data.length; i++) {
+      if(data[i]['type']=="text"){
+        print(data[i]['text']);
+        ChatMessage message = new ChatMessage(
+          text:data[i]['text'] ,
+          name: "Bot",
+          type: false,
+        );
+        setState(() {
+          _messages.insert(0, message);
+        });
+      }
+    }
+    httpClient.close();
+
+    return reply;
+  }
+
+
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+    ChatMessage message = new ChatMessage(
+      text: text,
+      name: "User",
+      type: true,
+    );
+    setState(() {
+      _messages.insert(0, message);
+    });
+    //Response(text);
+    request(text);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Virtual Nurse"),
+        actions: <Widget>[
+          new IconButton(
+            icon: callIcon,
+            onPressed: (){
+
+            },
+          )
+        ],
+      ),
+      body: new Column(children: <Widget>[
+        new Flexible(
+            child: new ListView.builder(
+              padding: new EdgeInsets.all(8.0),
+              reverse: true,
+              itemBuilder: (_, int index) => _messages[index],
+              itemCount: _messages.length,
+            )),
+        new Divider(height: 1.0),
+        new Container(
+          decoration: new BoxDecoration(color: Theme.of(context).cardColor),
+          child: _buildTextComposer(),
+        ),
+      ]),
+    );
+  }
+}
+
+
+class ChatMessage extends StatelessWidget {
+  ChatMessage({this.text, this.name, this.type});
+
+  final String text;
+  final String name;
+  final bool type;
+
+  List<Widget> otherMessage(context) {
+    return <Widget>[
+      new Container(
+        margin: const EdgeInsets.only(right: 16.0),
+        child: new CircleAvatar(child: new Text('B')),
+      ),
+      new Expanded(
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            new Text(this.name,
+                style: new TextStyle(fontWeight: FontWeight.bold)),
+            new Container(
+              margin: const EdgeInsets.only(top: 5.0),
+              child: new Text(text),
+            ),
+          ],
+        ),
+      ),
+    ];
+  }
+  List<Widget> myMessage(context) {
+    return <Widget>[
+      new Expanded(
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            new Text(this.name, style: Theme.of(context).textTheme.subhead),
+            new Container(
+              margin: const EdgeInsets.only(top: 5.0),
+              child: new Text(text),
+            ),
+          ],
+        ),
+      ),
+      new Container(
+        margin: const EdgeInsets.only(left: 16.0),
+        child: new CircleAvatar(
+            child: new Text(
+              this.name[0],
+              style: new TextStyle(fontWeight: FontWeight.bold),
+            )),
+      ),
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      child: new Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: this.type ? myMessage(context) : otherMessage(context),
+      ),
+    );
+  }
+}
